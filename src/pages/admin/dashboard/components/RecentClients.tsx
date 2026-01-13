28ce9047cce43883ddf3831c26304c174d5cfe71
Matheus Vinícius <matheus.vinicius@authsoftsolutions.com>
2026-01-13 12:08:41 -0300

    feat: pagination added to dashboard

--- src/pages/admin/dashboard/components/RecentClients.tsx
+++ src/pages/admin/dashboard/components/RecentClients.tsx
@@ -9,6 +9,15 @@ import {
   TableHeader,
   TableRow,
 } from "@/atomic/mol.table/table.component";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/atomic/mol.pagination/pagination.component";
 
 const recentClients = [
   {
@@ -94,6 +103,33 @@ export const RecentClients = () => {
               ))}
             </TableBody>
           </Table>
+
+          <div className="mt-4">
+            <Pagination>
+              <PaginationContent>
+                <PaginationItem>
+                  <PaginationPrevious href="#" disabled />
+                </PaginationItem>
+                <PaginationItem>
+                  <PaginationLink href="#" isActive className="bg-brand-primary-medium text-white hover:bg-brand-primary-dark hover:text-white">
+                    1
+                  </PaginationLink>
+                </PaginationItem>
+                <PaginationItem>
+                  <PaginationLink href="#">2</PaginationLink>
+                </PaginationItem>
+                <PaginationItem>
+                  <PaginationEllipsis />
+                </PaginationItem>
+                <PaginationItem>
+                  <PaginationLink href="#">3</PaginationLink>
+                </PaginationItem>
+                <PaginationItem>
+                  <PaginationNext href="#" />
+                </PaginationItem>
+              </PaginationContent>
+            </Pagination>
+          </div>
         </CardContent>
       </Card>
     </div>
